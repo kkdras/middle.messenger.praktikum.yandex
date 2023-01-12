1b70b6e545a34617ef388a350f8697669d27dd65
Kozlov Konstantin <mojolkozlov73@gmail.com>
2023-01-12 18:07:45 +0300

    add client utilsi

---
+++ src/client/utils/index.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @param  {...(string | { string: any } | any[])} args
+ * @returns 
+ */
+export const classNames = (...args) => {
+   const calcObj = (obj) => Object
+      .entries(obj)
+      .filter(entry => !!entry[1])
+      .map(entry => entry[0])
+      .join(' ')
+
+   const calcArr = (arr) => arr
+      .filter(item => !!item)
+      .map(item => {
+         if (typeof item !== 'object')
+            return item.toString()
+         if (item.__proto__ === Object.prototype)
+            return calcObj(item)
+         if (item.__proto__ !== Array.apply.prototype)
+            return calcArr(item)
+         throw new Error('wrong type of args')
+      }).join(' ')
+
+   return calcArr(args).trim()
+}

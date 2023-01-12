4418ffa713e38f40fcad2025cae73a515ff75480
Kozlov Konstantin <mojolkozlov73@gmail.com>
2023-01-12 18:05:46 +0300

    config parcel

---
+++ src/server/index.js
@@ -0,0 +1,31 @@
+import express  from 'express';
+import { readFile } from 'node:fs';
+import path from 'node:path';
+const app = express();
+const PORT = 3000;
+
+const readFilePromisify = (path, encoding = 'utf-8') => new Promise((res, rej) => {
+   readFile(path, { encoding }, (err, data) => {
+      if (err) rej(err)
+      else res(data)
+   });
+});
+
+app.use(express.static(path.resolve(__dirname, '../../build' , 'client')));
+
+app.get('/*', async (req, res) => {
+   try {
+      const page = await readFilePromisify(path.resolve(__dirname, '../../', 'build/client/index.html'));
+      res.status(200).send(page);
+   } catch (e) {
+      res.status(500).send(e.message);
+   }
+});
+
+app.get('*', (res, req) => {
+   res.status(404).send('The page not found');
+});
+
+app.listen(PORT, () => {
+   console.log(`start listening server on ${PORT} port`);
+});

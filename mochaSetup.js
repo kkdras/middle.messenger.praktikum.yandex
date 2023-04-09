c7d5287a4a29acf9d5e4eeff4467928c83c46e32
Kozlov Konstantin <mojolkozlov73@gmail.com>
2023-04-09 15:48:24 +0300

    configurate test env

---
+++ mochaSetup.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const { JSDOM } = require('jsdom');
+
+const { window } = new JSDOM('<div id="app"></div>', {
+	url: 'http://localhost:3000'
+});
+
+global.window = window;
+global.document = window.document;
+global.DocumentFragment = window.DocumentFragment;
+global.createElement = window.createElement;
+global.history = window.history;
+
+require.extensions['.hbs'] = function (module, filename) {
+	module.exports = fs.readFileSync(filename, 'utf-8');
+};
+
+require.extensions['.scss'] = function () {
+	module.exports = () => ({});
+};

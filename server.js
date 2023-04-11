9871a61c2dbd98e2910aedc2badb8e12c4914652
Kozlov Konstantin <mojolkozlov73@gmail.com>
2023-04-11 18:56:07 +0300

    fix tests

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
 const express = require('express');
-const { readFile } = require('node:fs');
-const path =  require('node:path');
+const path = require('node:path');
 const app = express();
 const PORT = 3000;
 
-const ROOT = process.cwd();
+app.use(express.static(path.join(__dirname, 'dist')));
 
-const readFilePromisify = (path, encoding = 'utf-8') =>
-	new Promise((res, rej) => {
-		readFile(path, { encoding }, (err, data) => {
-			if (err) rej(err);
-			else res(data);
-		});
-	});
-
-app.use(express.static(path.resolve(ROOT, 'build')));
-
-app.get('/*', async (req, res) => {
-	try {
-		const page = await readFilePromisify(
-			path.resolve(ROOT, 'build/index.html')
-		);
-		res.status(200).send(page);
-	} catch (e) {
-		res.status(500).send(e.message);
-	}
-});
-
-app.get('*', (res, req) => {
-	res.status(404).send('The page not found');
+app.get('*', async (_req, res) => {
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
